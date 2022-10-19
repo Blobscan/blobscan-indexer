074058b679d3618cf862c19ad52f6d29e8567f6f
0xGabi <gabrielpk.18@gmail.com>
2022-10-19 18:56:23 +0000

    feat: only write to the database when there is blobs data

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const axios = require("axios")
-const { JsonRpcProviderWithFinalizedEvents } = require("./src/provider");
+const ethers = require("ethers");
 const { connectToDatabase } = require("./src/mongodb");
 const { calculateVersionedHash, getEIP4844Tx, getBlobTx, generateCollectionData } = require("./src/utils");
 
 const EXECUTION_NODE_RPC = process.env.EXECUTION_NODE_URL || "http://localhost:8545"
 const BEACON_NODE_RPC = process.env.BEACON_NODE_RPC || "http://localhost:3500"
 
-const provider = new JsonRpcProviderWithFinalizedEvents(EXECUTION_NODE_RPC)
+const provider = new ethers.providers.StaticJsonRpcProvider(EXECUTION_NODE_RPC)
 
 async function main() {
 
@@ -14,7 +14,7 @@ async function main() {
 
     const { db } = await connectToDatabase()
 
-    while(true) {
+    while (true) {
         const latestBlock = (
             await axios.get(
                 `${BEACON_NODE_RPC}/eth/v2/beacon/blocks/head`
@@ -46,9 +46,9 @@ async function main() {
                     currentExecutionBlock.transactions.map(txHash => getEIP4844Tx(provider, txHash)))).filter(
                         tx => tx.blobVersionedHashes && tx.blobVersionedHashes.length
                     )
-        
+
                 if (!blobTxs.length) {
-                    console.log(`No blobs found at slot ${currentSlot}`)
+                    console.log(`No blobs hashes found at slot ${currentSlot}`)
                 } else {
                     console.log(`Found ${blobTxs.length} at slot ${currentSlot}`)
 
@@ -57,50 +57,53 @@ async function main() {
                             `${BEACON_NODE_RPC}/eth/v1/blobs/sidecar/${currentSlot}`
                         )
                     ).data.data;
-        
-                    let insertFns = []
-        
-                    const blockDocument = {
-                        number: currentExecutionBlock.number,
-                        hash: currentExecutionBlock.hash,
-                        timestamp: currentExecutionBlock.timestamp,
-                        slot: currentSlot
-                    }
-        
-                    insertFns.push(generateCollectionData(db, "blocks", blockDocument))
-        
-                    blobTxs.forEach((tx, index) => {
-                        const txDocument = {
-                            hash: tx.hash,
-                            block: currentExecutionBlock.number,
-                            from: tx.from,
-                            to: tx.to,
-                            index
-                        }
-        
-                        insertFns.push(generateCollectionData(db, "txs", txDocument))
-                    })
-        
-                    sidecar.blobs.forEach((blob, index) => {
-                        const commitment = beaconBlock.message.body.blob_kzgs[index]
-                        const versionedHash = calculateVersionedHash(commitment)
-                        const tx = getBlobTx(blobTxs, versionedHash)
-        
-                        const blobDocument = {
-                            hash: versionedHash,
-                            commitment,
-                            data: blob,
-                            tx: tx.hash,
-                            index
-                        }
-        
-                        insertFns.push(generateCollectionData(db, "blobs", blobDocument))
-                    })
 
-                    console.log(`Inserting ${insertFns.length} documents for slot ${currentSlot}`)
-                    await Promise.all(insertFns)
+                    if (!sidecar.length) {
+                        console.log(`No blobs data found on sidecar at ${currentSlot}`)
+                    } else {
+                        console.log(`Found ${sidecar.length} at slot ${currentSlot}`)
+
+
+                        let insertFns = []
+
+                        const blockDocument = {
+                            ...currentExecutionBlock,
+                            slot: currentSlot
+                        }
+
+                        insertFns.push(generateCollectionData(db, "blocks", blockDocument))
+
+                        blobTxs.forEach((tx, index) => {
+                            const txDocument = {
+                                ...tx,
+                                index,
+                                block: currentExecutionBlock.number,
+                            }
+
+                            insertFns.push(generateCollectionData(db, "txs", txDocument))
+                        })
+
+                        sidecar.blobs.forEach((blob, index) => {
+                            const commitment = beaconBlock.message.body.blob_kzgs[index]
+                            const versionedHash = calculateVersionedHash(commitment)
+                            const tx = getBlobTx(blobTxs, versionedHash)
+
+                            const blobDocument = {
+                                hash: versionedHash,
+                                commitment,
+                                data: blob,
+                                tx: tx.hash,
+                                index
+                            }
+
+                            insertFns.push(generateCollectionData(db, "blobs", blobDocument))
+                        })
+
+                        console.log(`Inserting ${insertFns.length} documents for slot ${currentSlot}`)
+                        await Promise.all(insertFns)
+                    }
                 }
-            } catch(err) {
+            } catch (err) {
                 console.log(err)
             }
         }
